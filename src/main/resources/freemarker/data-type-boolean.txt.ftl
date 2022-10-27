f398e05007593fbbd271c144bcce35c3076cdce9
dororo <2414566866@qq.com>
2022-10-27 21:36:15 +0800

    [demo-freemarker]-测试布尔值;

---
+++ src/main/resources/freemarker/data-type-boolean.txt.ftl
@@ -0,0 +1,14 @@
+${testBoolean01?c}
+${testBoolean01?string}
+${testBoolean02?string("true时的字符串","false时的字符串")}
+
+
+<#if testBoolean01>
+    true时显示我
+    <#else>
+    false时显示我
+</#if>
+
+
+
+
--- src/test/java/com/dororo/future/JustTests.java
+++ src/test/java/com/dororo/future/JustTests.java
@@ -13,31 +13,33 @@ import org.junit.jupiter.api.Test;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 
 public class JustTests {
-    /**
-     * 输出到桌面文件夹
-     */
-//    private static String output = StrUtil.format("{}\\\\{}", FileSystemView.getFileSystemView().getHomeDirectory().getAbsolutePath(), "output");
+    //输出到桌面文件夹
+    //private static String output = StrUtil.format("{}\\\\{}", FileSystemView.getFileSystemView().getHomeDirectory().getAbsolutePath(), "output");
+    //输出到项目out目录方便查看
     private static String output = StrUtil.format("{}\\\\{}", SystemUtil.getUserInfo().getCurrentDir(), "out");
-    /**
-     * 指定目录获取模板引擎
-     */
+    //指定目录获取模板引擎
     private static TemplateEngine engine = TemplateUtil.createEngine(new TemplateConfig("freemarker", TemplateConfig.ResourceMode.CLASSPATH));
-
-    /**
-     * 根据ftl文件目录路径获取文件列表
-     */
+    //根据ftl文件目录路径获取文件列表
     private static List<File> ftlList = ListUtil.toList(FileUtil.ls(StrUtil.format("{}{}", SystemUtil.getUserInfo().getCurrentDir(), "src\\main\\resources\\freemarker")));
 
     @Test
     void test01() {
         ftlList.forEach(file -> {
+            //传参
+            HashMap<String, Object> bindingMap = new HashMap<>();
+            bindingMap.put("testBoolean01", true);//测试布尔值
+            bindingMap.put("testBoolean02", false);//测试布尔值
+
+
             String ftlName = FileUtil.getName(file);
             Template template = engine.getTemplate(ftlName);
             String target = output + "\\" + ftlName.replace(".ftl", "");
-            template.render(null, FileUtil.file(target));
+            //执行渲染
+            template.render(bindingMap, FileUtil.file(target));
         });
     }
 }
